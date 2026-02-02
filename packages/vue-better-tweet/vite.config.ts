c7a663885fb7327f638d09012ad3d327cee983ae
Adrien Zaganelli <adrienzaganelli@gmail.com>
2026-02-02 02:05:26 +0100

    closer dist structure to react project

--- packages/vue-better-tweet/vite.config.ts
+++ packages/vue-better-tweet/vite.config.ts
@@ -1,26 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import { resolve } from "node:path";
 
 const entries = {
   index: resolve(__dirname, "src/index.ts"),
   "index.client": resolve(__dirname, "src/index.client.ts"),
   api: resolve(__dirname, "src/api/index.ts"),
-  'twitter-theme/theme': resolve(__dirname, 'src/twitter-theme/theme.css')
 };
 
 export default defineConfig({
   plugins: [
     vue(),
-    libInjectCss()
+    libInjectCss(),
   ],
   build: {
     // Avoid minified JS var collisions in CSS module output when bundled by other tools.
     minify: false,
     sourcemap: true,
-    outDir: 'dist',
-    // cssCodeSplit: true,
+    outDir: "dist",
+    cssCodeSplit: true,
     lib: {
       entry: entries,
       formats: ["es"],
@@ -29,6 +28,9 @@ export default defineConfig({
     rollupOptions: {
       external: ["vue", "clsx", "swrv"],
       output: {
+        // preserve to mach swc behavior fromm original react project
+        preserveModules: true,
+        preserveModulesRoot: "src",
         entryFileNames: "[name].js",
         chunkFileNames: "chunks/[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash][extname]",
